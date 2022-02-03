e7f7d01f0e55e9f93733c99900169c11e139acd8
Daniela <danisalazarcueto7@gmail.com>
2022-02-03 17:51:21 +0000

    Finish exercice 3 with some variations

--- Array-Bucles-2/ejercicio3.js
+++ Array-Bucles-2/ejercicio3.js
@@ -1,1 +1,23 @@
 'use strict'; 
+
+//ejercicio 3: Crear una función que devuelva algunos números del array de "Los números de la serie Lost" 
+
+const lostNumbers = [4, 8, 15, 16, 23, 42]; 
+
+function bestLostNumber(lostNumbers){
+    let evenLostNumbers = [];
+    let oddLostNumbers = [];
+    for(let lostNumber of lostNumbers){
+        if(lostNumber %2 === 0){
+            evenLostNumbers.push(lostNumber)
+        }else if(lostNumber % 3 === 0){
+            oddLostNumbers.push(lostNumber);
+        }
+    }
+    //const resultOfConcat =  evenLostNumbers.concat(oddLostNumbers);
+    return {evenLostNumbers, oddLostNumbers}; 
+}
+
+const {evenLostNumbers, oddLostNumbers} = bestLostNumber(lostNumbers)
+console.log(evenLostNumbers)
+console.log(oddLostNumbers)
